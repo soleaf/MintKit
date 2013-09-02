d90240a26f9dff7cb79937262740d5abd0a3f43a
soleaf <soleaf@gmail.com>
2013-09-02 11:53:37 +0900

    UIButton+NSDictionary

---
+++ mintKit/UIButton+NSDictionary.m
@@ -0,0 +1,31 @@
+//
+//  UIButton+NSDictionary.m
+//  mintKit
+//
+//  Created by soleaf on 13. 9. 2..
+//  Copyright (c) 2013년 mintcode.org. All rights reserved.
+//
+
+#import "UIButton+NSDictionary.h"
+
+@implementation UIButton(NSDIctionary)
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
