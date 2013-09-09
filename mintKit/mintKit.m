44b283e6b59de9af6d7084f39ae26bafb1369602
soleaf <soleaf@gmail.com>
2013-09-09 10:23:06 +0900

    Build 6

--- mintKit/mintKit.m
+++ mintKit/mintKit.m
@@ -9,9 +9,9 @@
 #import "mintKit.h"
 
 #define MINTKIT_VERSION            @"0.0.1"
-#define MINTKIT_BUILD              5
+#define MINTKIT_BUILD              6
 #define MINTKIT_RELEASE            0
-#define MINTKIT_VERSION_DATE       @"2013.09.04"
+#define MINTKIT_VERSION_DATE       @"2013.09.09"
 
 @implementation mintKit
 
