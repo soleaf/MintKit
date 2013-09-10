a96f812934afbe8d2283abdeea796465410169c2
soleaf <soleaf@gmail.com>
2013-09-10 14:24:01 +0900

    Sync build number

--- mintKit/mintKit/mintKit.m
+++ mintKit/mintKit/mintKit.m
@@ -9,7 +9,7 @@
 #import "mintKit.h"
 
 #define MINTKIT_VERSION            @"0.0.1"
-#define MINTKIT_BUILD              6
+#define MINTKIT_BUILD              35
 #define MINTKIT_RELEASE            0
 #define MINTKIT_VERSION_DATE       @"2013.09.09"
 
