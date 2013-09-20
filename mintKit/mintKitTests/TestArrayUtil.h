297049da19ec35104c9f2e905f612955471ed845
soleaf <soleaf@gmail.com>
2013-09-20 11:17:49 +0900

    SenTestCase TestArrayUtil Added

---
+++ mintKit/mintKitTests/TestArrayUtil.h
@@ -0,0 +1,17 @@
+//
+//  TestArrayUtil.h
+//  mintKit
+//
+//  Created by soleaf on 13. 9. 20..
+//  Copyright (c) 2013년 mintcode.org. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+/*
+ ArrayUtil Test
+ */
+
+@interface TestArrayUtil : SenTestCase
+
+@end
