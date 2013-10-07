321d77fde06ce5950d0ea94171e2429b83833b22
soleaf <soleaf@gmail.com>
2013-10-07 16:01:58 +0900

    Build 53

---
+++ mintKit/mintKit/BuildSupport.m
@@ -0,0 +1,13 @@
+//
+//  BuildSupport.m
+//  mintKit
+//
+//  Created by soleaf on 13. 10. 7..
+//  Copyright (c) 2013년 mintcode.org. All rights reserved.
+//
+
+#import "BuildSupport.h"
+
+@implementation BuildSupport
+
+@end
