87149db4f02cadf072b3d06ade9bc8360cc396be
soleaf <soleaf@gmail.com>
2013-11-25 14:29:14 +0900

    Formating

--- mintKit/mintKit/DateTimeSet.h
+++ mintKit/mintKit/DateTimeSet.h
@@ -38,6 +38,7 @@
 
 + (DateTimeSet*)makeDateTimeSetYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day;
 + (DateTimeSet*)makeDateTimeSetHour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second;
-+ (DateTimeSet*)makeDateTimeSetYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day hour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second;
++ (DateTimeSet*)makeDateTimeSetYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day
+                               hour:(NSInteger)hour minute:(NSInteger)minute second:(NSInteger)second;
 
 @end
